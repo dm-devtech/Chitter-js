350f690b063f0a4019bc8321126e9168bbdd798b
dm-devtech <70208036+dm-devtech@users.noreply.github.com>
2021-01-22 16:34:08 +0000

    setting up views folder

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const port = 3003
 
 app.get("/", (req, res, next) => {
   // request variable incoming, response variable sending back to user and next which handles middleware
+  app.set("view engine", "pug") // tell server which template engine to use
+  app.set("views", "views") // tell server where to look to find view files, when you need a view go to views folder
   res.status(200).send("confirmed") // when access code this will run
 })
 
